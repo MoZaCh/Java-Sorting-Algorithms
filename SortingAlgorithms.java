92a759fdd9228e479a85212e8d8681405f7be15d
Mohammed Choudhury <mzchoudhury@outlook.com>
2021-06-18 16:49:44 +0100

    Bubble Sort Implemented

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -11,6 +11,12 @@ package sorting.algorithms;
  */
 public class SortingAlgorithms {
 
+    public static void printArray(int[] array) {
+        for(int i=0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+    
     /**
      * 
      * @param array
@@ -18,8 +24,21 @@ public class SortingAlgorithms {
      */
     public static int[] bubbleSort(int[] array) {
         
+        int i, j, temp = 0;
         
-        
+        for(i =0; i < array.length -1; i++ ) {
+
+            for(j=0; j < array.length -1 -i; j++) {
+                
+                if(array[j] > array[j+1]) {
+                    temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                    System.out.println("Iteration: " + i);
+                    printArray(array);
+                }
+            }
+        }
         return array;
     }
     
@@ -28,7 +47,14 @@ public class SortingAlgorithms {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int[] array = {5, 8, 1, 6, 9, 2};
+        int[] result = bubbleSort(array);
+        
+        System.out.println("Result:");
+        for(int i=0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
     }
     
 }
